082dbf2735a5a23418080e61ecb046bd68f678da
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2023-04-19 21:11:36 -0500

    dynamic shebang, shellcheck exclusion, indent via tabs

--- clean_launch.sh
+++ clean_launch.sh
@@ -1,18 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# shellcheck disable=SC2164
 
 rm -rf /var/run/sudo/* /tmp/* /var/tmp/*
 rm -f /run/* 2>/dev/null
 
-if [ ! -d "/etc/dropbear" ]; then
-  mkdir /etc/dropbear
-  /usr/bin/dropbearkey -t ecdsa -f /etc/dropbear/dropbear_ecdsa_host_key
+if [[ ! -d "/etc/dropbear" ]]; then
+	mkdir /etc/dropbear
+	/usr/bin/dropbearkey -t ecdsa -f /etc/dropbear/dropbear_ecdsa_host_key
 fi
 
-if [ ! -f "/etc/xrdp/rsakeys.ini" ]; then
-  /usr/bin/xrdp-keygen xrdp auto 2048
-  pushd /etc/xrdp &>/dev/null
-  rm -f cert.pem key.pem
-  openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -subj "/C=XX/ST=XX/L=XX/O=XX/CN=$(hostname)" -keyout key.pem -out cert.pem
+if [[ ! -f "/etc/xrdp/rsakeys.ini" ]]; then
+	/usr/bin/xrdp-keygen xrdp auto 2048
+	pushd /etc/xrdp &>/dev/null
+	rm -f cert.pem key.pem
+	openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -subj "/C=XX/ST=XX/L=XX/O=XX/CN=$(hostname)" -keyout key.pem -out cert.pem
 fi
 
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
